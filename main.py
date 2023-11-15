b5ebae5c07205111aecc947dd0175d8b6787aa99
Josué García <77043114@continental.edu.pe>
2023-11-15 12:22:03 -0500

    Actualización de main

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ def solucionarEcuacion (coef1, coef2, coef3):
     print(f"Raíz 1: {raiz1}\nRaíz 2: {raiz2}")
 
 if __name__ == '__main__':
+    print("Ecuación 1:")
     solucionarEcuacion(1, 1, 1)
+    print("\nEcuación 2:")
+    solucionarEcuacion(1, 2, 1)
