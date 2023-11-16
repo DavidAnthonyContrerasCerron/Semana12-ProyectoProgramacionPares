974f2d0c05a1dfc13bf9d47617159261bf9ca08c
Josué García <77043114@continental.edu.pe>
2023-11-15 22:50:12 -0500

    Se agregó más pruebas y correción de la función core

--- src/logica/EcuacionSegundoGrado.py
+++ src/logica/EcuacionSegundoGrado.py
@@ -6,11 +6,7 @@ class ExceptionDatos(Exception):
 
 class EcuacionSegundoGrado:
     def __init__(self, coeficientes):
-        if coeficientes is not None:
-            self.__coeficientes = self.validarCoeficientes(coeficientes)
-        else:
-            raise ExceptionDatos("Se requieren tres coeficientes.")
-
+        self.__coeficientes = self.validarCoeficientes(coeficientes)
         self.raiz1 = 0
         self.raiz2 = 0
 
@@ -42,27 +38,18 @@ class EcuacionSegundoGrado:
         discriminante = b ** 2 - 4 * a * c
 
         if discriminante > 0:
-            # Dos raíces reales distintas
             Raiz1 = (-b + cmath.sqrt(discriminante)) / (2 * a)
             Raiz2 = (-b - cmath.sqrt(discriminante)) / (2 * a)
-            self.raiz1 = format(Raiz1.real, '.2f')
-            self.raiz2 = format(Raiz2.real, '.2f')
-            return self.raiz1, self.raiz2
+            return Raiz1.real, Raiz2.real
 
         elif discriminante == 0:
-            # Una raíz real (doble)
-
             Raiz1 = -b / (2 * a)
-            self.raiz1 = format(Raiz1.real, '.2f')
-            self.raiz2 = self.raiz1
-            return self.raiz1, self.raiz2
+            return Raiz1.real, Raiz1.real
+
         else:
-            # Dos raíces complejas conjugadas
             Raiz1 = (-b + cmath.sqrt(discriminante)) / (2 * a)
             Raiz2 = (-b - cmath.sqrt(discriminante)) / (2 * a)
-            self.raiz1 = round(Raiz1.real, 2) + round(Raiz1.imag, 2) * 1j
-            self.raiz2 = round(Raiz2.real, 2) + round(Raiz2.imag, 2) * 1j
-            return self.raiz1, self.raiz2
+            return Raiz1, Raiz2
 
     def imprimir_raices(self):
         print(f"Raiz 1: {self.raiz1}\nRaiz 2: {self.raiz2}")
--- tests/PruebaRaices.py
+++ tests/PruebaRaices.py
@@ -4,27 +4,38 @@ from src.logica.EcuacionSegundoGrado import EcuacionSegundoGrado, ExceptionDatos
 
 class PruebaRaices(unittest.TestCase):
     def setUp(self):
-        self.ecuacionSegundoGrado = EcuacionSegundoGrado([])
+        self.ecuacionSegundoGrado = EcuacionSegundoGrado([3, -5, 1])
 
     def tearDown(self):
         self.ecuacionSegundoGrado = None
 
     def test_TresCoeficientesEnteros_retornaRaicesReales(self):
-        self.ecuacionSegundoGrado.coeficientes = []
-
         # Arrange
         self.ecuacionSegundoGrado.coeficientes = [3, -5, 1]
         raiz1 = 1.43
         raiz2 = 0.23
 
         # Do
-        raiz1Actual, raiz2Actual = self.ecuacionSegundoGrado.calcular_raices()
-
-        # Assert
-        self.assertAlmostEqual(raiz1Actual, raiz1, places=2)
-        self.assertAlmostEqual(raiz2Actual, raiz2, places=2)
         raiz1Calculada, raiz2Calculada = self.ecuacionSegundoGrado.calcular_raices()
 
         # Assert
         self.assertAlmostEqual(raiz1Calculada, raiz1, places=2)
         self.assertAlmostEqual(raiz2Calculada, raiz2, places=2)
+
+    def test_TresCoeficientesNoNumericos_lanzaException(self):
+        # Act y Assert
+        with self.assertRaises(ExceptionDatos):
+            self.ecuacionSegundoGrado.coeficientes = ["a", "b", "c"]
+
+    def test_RaicesComplejas_retornaRaicesComplejas(self):
+        # Arrange
+        self.ecuacionSegundoGrado.coeficientes = [1, 0, 1]
+        raiz1 = complex(0, 1)
+        raiz2 = complex(0, -1)
+
+        # Do
+        raiz1Calculada, raiz2Calculada = self.ecuacionSegundoGrado.calcular_raices()
+
+        # Assert
+        self.assertEqual(raiz1Calculada, raiz1)
+        self.assertEqual(raiz2Calculada, raiz2)
